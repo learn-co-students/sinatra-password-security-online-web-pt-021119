efa605907e1e1fd8af78366b471adf3acaa866e4
github username <github email address>
2019-05-30 12:08:51 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20180314210852) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "username"
     t.string "password_digest"
   end
